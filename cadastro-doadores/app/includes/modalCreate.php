777b5a02be98bc747a8a2915ea92cac0e5a9045f
Rafael R Doimo <80365201+raf-rodrigo@users.noreply.github.com>
2025-01-29 07:15:21 -0300

    Atualizar o modalCreate.php

--- cadastro-doadores/app/includes/modalCreate.php
+++ cadastro-doadores/app/includes/modalCreate.php
@@ -11,79 +11,82 @@
                             <input type="text" id="nome" name="nome" class="form-control" placeholder="Nome" required/>
                         </div>
                         <div class="form-group col">
-                            <input type="email" id="email" name="email" class="form-control" placeholder="" required/>
+                            <input type="email" id="email" name="email" class="form-control" placeholder="email" required/>
+                        </div>
+                    </div>
+t
+                    <div class="row my-2">
+                        <div class="form-group col">
+                            <input type="text" id="cpf" name="cpf" class="form-control" placeholder="CPF" required/>
+                        </div>
+                        <div class="form-group col">
+                            <input type="text" id="telefone" name="telefone" class="form-control" placeholder=Telefone"" required/>
                         </div>
                     </div>
 
                     <div class="row my-2">
                         <div class="form-group col">
-                            <input type="text" id="cpf" name="cpf" class="form-control" placeholder="" required/>
+                            <input type="text" id="data_nascimento" name="data_nascimento" class="form-control" placeholder="Data de Nascimento" required/>
                         </div>
                         <div class="form-group col">
-                            <input type="text" id="telefone" name="telefone" class="form-control" placeholder="" required/>
-                        </div>
-                    </div>
-
-                    <div class="row my-2">
-                        <div class="form-group col">
-                            <input type="text" id="data_nascimento" name="data_nascimento" class="form-control" placeholder="" required/>
-                        </div>
-                        <div class="form-group col">
-                            <input type="text" id="data_cadastro" name="data_cadastro" class="form-control" placeholder="" required/>
+                            <input type="text" id="data_cadastro" name="data_cadastro" class="form-control" placeholder="Data de Cadastro" required/>
                         </div>
                     </div>
 
                     <div class="row my-2">
                         <div class="form-group col">
-                            <input type="text" id="logradouro" name="logradouro" class="form-control" placeholder="" required/>
+                            <input type="text" id="cep" name="cep" class="form-control" placeholder="CEP" required/>
                         </div>
                         <div class="form-group col">
-                            <input type="text" id="numero" name="numero" class="form-control" placeholder="" required/>
-                        </div>
-                    </div>
-
-                    <div class="row my-2">
-                        <div class="form-group col">
-                            <input type="text" id="complemento" name="complemento" class="form-control" placeholder="" required/>
+                            <input type="text" id="logradouro" name="logradouro" class="form-control" placeholder="Logradouro" required/>
                         </div>
                         <div class="form-group col">
-                            <input type="text" id="bairro" name="bairro" class="form-control" placeholder="" required/>
+                            <input type="text" id="numero" name="numero" class="form-control" placeholder="Número" required/>
                         </div>
                     </div>
 
                     <div class="row my-2">
                         <div class="form-group col">
-                            <input type="text" id="cidade" name="cidade" class="form-control" placeholder="" required/>
+                            <input type="text" id="complemento" name="complemento" class="form-control" placeholder="Complemento" required/>
                         </div>
                         <div class="form-group col">
-                            <input type="text" id="estado" name="estado" class="form-control" placeholder="" required/>
-                        </div>
-                    </div>
-
-                    <div class="row my-2">
-                        <div class="form-group col">
-                            <input type="text" id="intervalo_doacao" name="intervalo_doacao" class="form-control" placeholder="" required/>
-                        </div>
-                        <div class="form-group col">
-                            <input type="text" id="valor_doacao" name="valor_doacao" class="form-control" placeholder="" required/>
+                            <input type="text" id="bairro" name="bairro" class="form-control" placeholder="Bairro" required/>
                         </div>
                     </div>
 
                     <div class="row my-2">
                         <div class="form-group col">
-                            <input type="text" id="forma_pagamento" name="forma_pagamento" class="form-control" placeholder="" required/>
+                            <input type="text" id="cidade" name="cidade" class="form-control" placeholder="Cidade" required/>
                         </div>
                         <div class="form-group col">
-                            <input type="text" id="banco" name="banco" class="form-control" placeholder="" required/>
+                            <input type="text" id="estado" name="estado" class="form-control" placeholder="Estado" required/>
                         </div>
                     </div>
 
                     <div class="row my-2">
                         <div class="form-group col">
-                            <input type="text" id="agencia" name="agencia" class="form-control" placeholder="" required/>
+                            <input type="text" id="intervalo_doacao" name="intervalo_doacao" class="form-control" placeholder="Intervalo de Doação" required/>
                         </div>
                         <div class="form-group col">
-                            <input type="text" id="conta" name="conta" class="form-control" placeholder="" required/>
+                            <input type="text" id="valor_doacao" name="valor_doacao" class="form-control" placeholder="Valor Doação" required/>
+                        </div>
+                    </div>
+
+                    <div class="row my-2">
+                        <div class="form-group col">
+                            <input type="text" id="forma_pagamento" name="forma_pagamento" class="form-control" placeholder="Forma de Pagamento" required/>
+                        </div>
+                        <div class="form-group col">
+                            <input type="text" id="banco" name="banco" class="form-control" placeholder="Banco" required/>
+                        </div>
+                    </div>
+
+                    <div class="row my-2">
+                        <div class="form-group col">
+                            <input type="text" id="agencia" name="agencia" class="form-control" placeholder="Agencia" required/>
+                        </div>
+                        <div class="form-group col">
+                            <input type="text" id="conta" name="conta" class="form-control" placeholder="Conta" required/>
                         </div>
                     </div>
                     <div class="row my-2">
